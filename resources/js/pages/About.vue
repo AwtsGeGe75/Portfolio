4750f3701a62d08f990e33c5f920ac11caf980d8
jmBaldano <baldanojohnmark@gmail.com>
2025-08-12 23:54:59 +0800

    about me content

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -13,10 +13,11 @@
     <div class="about">
        
     <div class="sections">
-      <h1 class="text-3xl font-bold text-center text-white mb-6 py-16">
+      <h1 class="text-3xl font-bold text-center text-white  py-10">
         About Me
       </h1>
-      <p class =" text-base text-white text-center px-4 ">
+      <div class=" ">
+      <p class =" font-medium  text- text-white text-center mx-10 px-15">
         Graduate of Bachelor of Science in Information Technology Specialized in Web and Mobile application with a strong foundation in web development, particularly
 using Laravel, Vue.js, Tailwind CSS, and UI/UX design with Figma. I have a deep curiosity for
 technology and a commitment to lifelong learning. I was part of an award-winning team recognized
@@ -24,6 +25,7 @@ for Best Web Application at TICap 18. I am now seeking an opportunity to be part
 development project where I can apply and further develop my knowledge and skills.
 
       </p>
+      </div>
   </div>
     </div>
     
